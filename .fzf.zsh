51f6535633f404b1aa013b267e961003d0c57bd9
PT1191 <pt1191@PT1191noMacBook-Pro.local>
2019-11-21 12:44:27 +0900

    fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/baban/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/baban/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/baban/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/baban/.fzf/shell/key-bindings.zsh"
+source "~/.fzf/shell/key-bindings.zsh"
