2ed65a041b54366e89f532a50cdbeceddd7c1a52
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-12-19 12:17:58 +0900

    myhome

--- home-setup.sh
+++ home-setup.sh
@@ -3,3 +3,8 @@
 # gitからリポジトリを取り出し
 git checkout-index -a -f --prefix=~/
 
+
+# fzfコマンドをinstall
+# https://qiita.com/tdrk/items/1eb27555ad65d63a6a25
+git clone https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
